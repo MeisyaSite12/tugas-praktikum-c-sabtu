447c81c87feaa2ae250de0f5623d460598671614
Meisya Salsabila <salsabilam911@gmail.com>
2024-09-21 18:45:40 +0700

    first commit

---
+++ tugas-1/tugas-mandiri-1.cpp
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+int main(){
+	printf("NIM  : 2441026 \n");
+	printf("NAMA : MEISYA SALSABILA \n");
+	printf("JURUSAN : TEKNIK INFORMATIKA");
+	
+	return 0;
+}
---
+++ tugas-1/tugas-mandiri-2.cpp
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+int main(){
+	int panjang, lebar;
+	panjang = 10;
+	lebar = 7;
+	printf("Total luas persegi panjang adalah %i cm", (panjang*lebar));
+	
+	return 0;
+}
